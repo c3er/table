aac608d3ac6b8782c672915ff3611199cd91c7a6
flipperx <flipperx@b44006b2-7ebe-364d-865b-74746e22be69>
2010-09-03 00:46:09 +0000

    git-svn-id: https://ada/svn/table@9 b44006b2-7ebe-364d-865b-74746e22be69

--- todo.mm
+++ todo.mm
@@ -4,17 +4,12 @@
 <hook NAME="MapStyle" max_node_width="600"/>
 <node TEXT="Fehlende Features" POSITION="right" ID="_" CREATED="1282951745400" MODIFIED="1282951753257">
 <node TEXT="Asiabookie einlesen" ID="Freemind_Link_1346968996" CREATED="1282951763706" MODIFIED="1282951774560">
-<node TEXT="DOM muss eingelesen werden" ID="Freemind_Link_1022609593" CREATED="1282951782785" MODIFIED="1282951793615">
-<node TEXT="C# benutzen" ID="Freemind_Link_521940842" CREATED="1282951798591" MODIFIED="1282951841468">
-<node TEXT="WebBrowser Objekt einsetzen" ID="Freemind_Link_383300692" CREATED="1282951850222" MODIFIED="1282951873479"/>
-<node TEXT="Feinschliff durchf&#xfc;hren" ID="ID_586805536" CREATED="1283366969323" MODIFIED="1283366984182"/>
-</node>
-</node>
 <node TEXT="Mehrere HTML Seiten einlesen und in einzelne Tabelle speichern" ID="Freemind_Link_314297537" CREATED="1282951901600" MODIFIED="1282951933477"/>
 </node>
 </node>
 <node TEXT="Architektur" POSITION="right" ID="Freemind_Link_1026043641" CREATED="1282952316449" MODIFIED="1282952337394">
-<node TEXT="Anwendung muss aufgespalten werden" ID="Freemind_Link_1950399688" CREATED="1282952340170" MODIFIED="1282952357648">
+<node TEXT="Anwendung muss aufgespalten werden" ID="Freemind_Link_1950399688" CREATED="1282952340170" MODIFIED="1283454430766">
+<icon BUILTIN="help"/>
 <node TEXT="Daten" ID="Freemind_Link_484839286" CREATED="1282952362499" MODIFIED="1282952393947"/>
 <node TEXT="Parser" ID="Freemind_Link_321753508" CREATED="1282952395619" MODIFIED="1282952400230"/>
 <node TEXT="GUI" ID="Freemind_Link_97096024" CREATED="1282952406962" MODIFIED="1282952414067"/>
