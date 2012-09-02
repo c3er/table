472a635fc29c11028cc8f8d4ced8771e3cc1abc0
flipperx <flipperx@b44006b2-7ebe-364d-865b-74746e22be69>
2012-09-02 11:43:02 +0000

    git-svn-id: https://ada/svn/table@46 b44006b2-7ebe-364d-865b-74746e22be69

---
+++ tabellenauswerter/testmodule.py
@@ -0,0 +1,35 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import table
+
+def test_split_data():
+    print('Function "split_data"')
+    
+    print(table.split_data('100'))
+    print(table.split_data('-100'))
+    print(table.split_data('- 100'))
+    
+    print(table.split_data('100.5'))
+    print(table.split_data('-100.5'))
+    print(table.split_data('- 100.5'))
+    
+    print(table.split_data('100%'))
+    print(table.split_data('-100%'))
+    print(table.split_data('- 100%'))
+    
+    print(table.split_data('100.5%'))
+    print(table.split_data('-100.5%'))
+    print(table.split_data('- 100.5%'))
+    
+    print(table.split_data('100.5 %'))
+    print(table.split_data('-100.5 %'))
+    print(table.split_data('- 100.5 %'))
+    
+    print(table.split_data('hallo'))
+    print(table.split_data('hallo123'))
+    
+    print()
+
+if __name__ == '__main__':
+    test_split_data()
