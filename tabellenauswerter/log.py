38f337edb2901aa6452193c7826527a418e607f5
Christian Dreier <flipper.x3r@googlemail.com>
2016-12-08 19:10:15 +0100

    Some reformating

--- tabellenauswerter/log.py
+++ tabellenauswerter/log.py
@@ -7,14 +7,17 @@ my application.
 Copied from "The Python Rag" - August 2009
 '''
 
+
 import logging
 import logging.handlers
 from functools import wraps
+
 
 # Create a global logger
 _vs_logger = None
 _handler = None
 _debug_on = None
+
 
 def init(logfile, debug_on = False):
     '''Sets up the logger.
@@ -37,14 +40,16 @@ def init(logfile, debug_on = False):
     form = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
     # Add the log message handler to the logger
-    _handler = logging.handlers.RotatingFileHandler(logfile,
-        maxBytes = 1048576,  # 1 MB
-        backupCount = 5
+    _handler = logging.handlers.RotatingFileHandler(
+        logfile,
+        maxBytes=1048576,  # 1 MB
+        backupCount=5
     )
 
     _handler.setFormatter(form)
     _vs_logger.addHandler(_handler)
 
+    
 def close():
     '''Closes the logger'''
     global _handler
@@ -52,30 +57,36 @@ def close():
     _handler.close()
     _vs_logger = None
 
+    
 def isready():
     '''Returns the status of the logger.'''
     return _vs_logger is not None
 
+    
 def info(msg):
     '''Log message with level info.'''
     if _vs_logger:
         _vs_logger.info(str(msg))
 
+        
 def debug(msg):
     '''Log message with level debug.'''
     if _debug_on and _vs_logger:
         _vs_logger.debug(str(msg))
 
+        
 def error(msg):
     '''Log message with level error.'''
     if _vs_logger:
         _vs_logger.error(str(msg))
 
+        
 def exception(msg):
     '''Log message with level error plus exception traceback.'''
     if _vs_logger:
         _vs_logger.exception(str(msg))
 
+        
 def logfunction(f):
     '''Creates a decorator to log a function.'''
     @wraps(f)
@@ -84,6 +95,7 @@ def logfunction(f):
         return f(*args, **kw)
     return wrapper
 
+    
 def logmethod(m):
     '''Creates a decorator to log a method.'''
     @wraps(m)
